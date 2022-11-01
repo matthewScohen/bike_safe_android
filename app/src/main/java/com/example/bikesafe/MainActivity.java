edea7f666ffba44db0ed60165a6f59ebc9426fe0
matthewScohen <64049126+matthewScohen@users.noreply.github.com>
2022-11-01 13:10:19 -0400

    app now connects to microcontroller and sends phone number via bluetooth on set phone function call

--- app/src/main/java/com/example/bikesafe/MainActivity.java
+++ app/src/main/java/com/example/bikesafe/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.bikesafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -15,66 +25,55 @@ import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
+
 public class MainActivity<LeDeviceListAdapter> extends AppCompatActivity {
 
     TextView textView;
     EditText phoneNum;
 
-    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-//    private LeDeviceListAdapter leDeviceListAdapter = new LeDeviceListAdapter();
-
-    // Device scan callback.
-//    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
-//        @Override
-//        public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
-//            System.out.println(bluetoothDevice);
-//        }
-//    };
-
-//    private LeDeviceListAdapter leDeviceListAdapter = new LeDeviceListAdapter();
-
-    //     Device scan callback.
-    private ScanCallback leScanCallback =
-            new ScanCallback() {
-                @Override
-                public void onScanResult(int callbackType, ScanResult result) {
-                    super.onScanResult(callbackType, result);
-//                    leDeviceListAdapter.addDevice(result.getDevice());
-//                    leDeviceListAdapter.notifyDataSetChanged();
-                    Log.d("TEST", "MESSAGE2");
-                    Log.d("IDENTIFIER",result.toString());
+    private BluetoothGatt ble_gatt = null;
+    private final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(final BluetoothGatt gatt, final int status, final int newState) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    // We successfully connected, proceed with service discovery
+                    Log.d("bike_safe_only", "device connected");
+                    gatt.discoverServices();
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    // We successfully disconnected on our own request
+                    gatt.close();
+                } else {
+                    // We're CONNECTING or DISCONNECTING, ignore for now
                 }
-            };
-
-    private BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-    private boolean scanning;
-    private Handler handler = new Handler();
-
-    // Stops scanning after 10 seconds.
-    private static final long SCAN_PERIOD = 10000;
-
-    private void scanLeDevice() {
-        if (!scanning) {
-            // Stops scanning after a predefined scan period.
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    scanning = false;
-                    Log.d("TEST", "STOP");
-                    bluetoothLeScanner.stopScan(leScanCallback);
-                }
-            }, SCAN_PERIOD);
-
-            scanning = true;
-            Log.d("TEST", "START");
-            bluetoothLeScanner.startScan(leScanCallback);
-        } else {
-            scanning = false;
-            Log.d("TEST", "STOP2");
-            bluetoothLeScanner.stopScan(leScanCallback);
+            } else {
+                // An error happened...figure out what happened!
+                gatt.close();
+            }
         }
-    }
+        @Override
+        public void onServicesDiscovered (BluetoothGatt gatt, int status)
+        {
+            ble_gatt = gatt;
+            // the 3rd service in the list is the UART service
+            UUID UART_UUID = gatt.getServices().get(2).getUuid();
+            BluetoothGattCharacteristic RX_CHAR = gatt.getServices().get(2).getCharacteristics().get(1);
+            UUID UART_WRITE_CHARACTERISTIC = RX_CHAR.getUuid();
+
+            Log.d("bike_safe_only", "found service " + UART_UUID);
+            Log.d("bike_safe_only", "found characteristic " + UART_WRITE_CHARACTERISTIC);
+
+//            byte[] message = phoneNum.getText().toString().getBytes(StandardCharsets.UTF_8);
+//            RX_CHAR.setValue(message);
+//            RX_CHAR.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+//            gatt.writeCharacteristic(RX_CHAR);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,12 +82,30 @@ public class MainActivity<LeDeviceListAdapter> extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textView);
         phoneNum = (EditText) findViewById(R.id.inputPhone);
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Get device object for a mac address
 
-//        scanLeDevice();   //This currently breaks the emulator, I think because of permissions
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice("58:BF:25:E7:14:5E");
+        // Check if the peripheral is cached or not
+        int deviceType = device.getType();
+        if(deviceType == BluetoothDevice.DEVICE_TYPE_UNKNOWN) {
+            Log.d("bike_safe_only", "device not cached");
+        } else {
+            Log.d("bike_safe_only", "device cached");
+        }
+
+        BluetoothGatt gatt = device.connectGatt(textView.getContext(), false, bluetoothGattCallback, BluetoothDevice.TRANSPORT_LE);
     }
 
     public void setPhone(View view){
-        Snackbar.make(view, "Button Pressed, Sending phone number \"" + phoneNum.getText() + "\"", Snackbar.LENGTH_LONG).show();
+        UUID UART_UUID = ble_gatt.getServices().get(2).getUuid();
+        BluetoothGattCharacteristic RX_CHAR = ble_gatt.getServices().get(2).getCharacteristics().get(1);
+        UUID UART_WRITE_CHARACTERISTIC = RX_CHAR.getUuid();
 
+        byte[] message = phoneNum.getText().toString().getBytes(StandardCharsets.UTF_8);
+        RX_CHAR.setValue(message);
+        RX_CHAR.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+        ble_gatt.writeCharacteristic(RX_CHAR);
+        Snackbar.make(view, "Button Pressed, Sending phone number \"" + phoneNum.getText() + "\"", Snackbar.LENGTH_LONG).show();
     }
 }
