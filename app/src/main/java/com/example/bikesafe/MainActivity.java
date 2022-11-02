443b7223e71ad29258915500abe4c4b3deeebce4
nickstrawser <n.strawser@ufl.edu>
2022-11-02 17:51:26 -0400

    added phone number validation

--- app/src/main/java/com/example/bikesafe/MainActivity.java
+++ app/src/main/java/com/example/bikesafe/MainActivity.java
@@ -98,14 +98,55 @@ public class MainActivity<LeDeviceListAdapter> extends AppCompatActivity {
     }
 
     public void setPhone(View view){
+
+        //verify input first
+        if(!validPhone()){
+            Snackbar.make(view, "INVALID PHONE NUMBER: Please enter number in the form XXX-XXX-XXXX or XXXXXXXXXX", Snackbar.LENGTH_LONG).show();
+            return;
+        }
+        Snackbar.make(view, "Button Pressed, Sending phone number \"" + phoneNum.getText() + "\"", Snackbar.LENGTH_LONG).show();
+
         UUID UART_UUID = ble_gatt.getServices().get(2).getUuid();
         BluetoothGattCharacteristic RX_CHAR = ble_gatt.getServices().get(2).getCharacteristics().get(1);
         UUID UART_WRITE_CHARACTERISTIC = RX_CHAR.getUuid();
 
-        byte[] message = phoneNum.getText().toString().getBytes(StandardCharsets.UTF_8);
+        byte[] message = getMessage();
+        Log.d("Phone", new String(message, StandardCharsets.UTF_8));
         RX_CHAR.setValue(message);
         RX_CHAR.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
         ble_gatt.writeCharacteristic(RX_CHAR);
-        Snackbar.make(view, "Button Pressed, Sending phone number \"" + phoneNum.getText() + "\"", Snackbar.LENGTH_LONG).show();
+    }
+
+    public byte[] getMessage(){
+        String number = phoneNum.getText().toString();
+        if(number.length() == 12){
+            number = number.substring(0,3) + number.substring(4,7) + number.substring(8);
+        }
+        return number.getBytes(StandardCharsets.UTF_8);
+    }
+
+    public boolean validPhone(){
+        String number = phoneNum.getText().toString();
+        Log.d("PHONE", "starting check with number " + number);
+        Log.d("PHONE", "Length: " + number.length());
+        if(number.length() == 12){
+            Log.d("PHONE", "trying to remove hyphens");
+            // XXX-XXX-XXXX
+            //remove hyphens
+            number = number.substring(0,3) + number.substring(4,7) + number.substring(8);
+        }
+        if(number.length() == 10){
+            try{
+                long test = Long.parseLong(number);
+                if(test < 0){
+                    return false;
+                }
+            }
+            catch (Exception e){
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
